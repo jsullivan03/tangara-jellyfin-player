0975c2cdbdaf252eb6765b32d4c880911abfcc9f
Tom Kirchner <git@halffull.org>
2024-12-31 17:04:16 -0800

    Add track info screen

--- lua/images.lua
+++ lua/images.lua
@@ -23,6 +23,7 @@ local img = {
   usb = lvgl.ImgData("//lua/img/usb.png"),
   listened = lvgl.ImgData("//lua/img/listened.png"),
   unlistened = lvgl.ImgData("//lua/img/unlistened.png"),
+  info = lvgl.ImgData("//lua/img/info.png"),
 }
 
 return img
