db6239de60fba1f76e3d438c53f2e30bf1135638
River Tae Smith <22485304+r-tae@users.noreply.github.com>
2025-02-05 12:41:25 +1100

    Update to latest version of fusion12 and build it like 10px

--- tools/fonts/mkfonts.sh
+++ tools/fonts/mkfonts.sh
@@ -8,10 +8,13 @@
 # npm i lv_font_conv -g
 fusion_12() {
   lv_font_conv \
-    --font fusion/fusion-pixel-12px-proportional.ttf \
+    --font fusion/fusion-pixel-12px-proportional/fusion-pixel-12px-proportional-latin.ttf \
     -r 0x2000-0x206F \
     -r 0x20-0x7F,0xA0-0xFF \
-    -r 0x3000-0x303f,0x3040-0x309F,0x30A0-0x30FF,0xFF00-0xFFEF,0x4E00-0x9FAF \
+    --font fusion/fusion-pixel-12px-proportional/fusion-pixel-12px-proportional-ja.ttf \
+    -r 0x3000-0x303f,0x3040-0x309F,0x30A0-0x30FF \
+    -r 0xFF00-0xFFEF,0x4E00-0x9FAF \
+    --font fusion/fusion-pixel-12px-proportional/fusion-pixel-12px-proportional-ko.ttf \
     -r 0xAC00-0xD7AF \
     --size 12 \
     --bpp 1 --format bin -o fusion12
