4890c7915c9684f46209c2179f2fdcab7fa3d778
ayumi <ayumi@noreply.codeberg.org>
2025-04-05 20:58:33 +0200

    Add the missing WavPack decoder licence to the Licence screen
    Also unifies the WavPack and Xiph.org 3–clause BSD licences.

--- lua/licenses.lua
+++ lua/licenses.lua
@@ -62,19 +62,18 @@ THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH RE
 OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.]])
 end
 
-local function xiphbsd(copyright)
+local function bsd3(copyright, name)
   show_license(copyright .. [[
 
 Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
 
-- Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
-- Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
-- Neither the name of the Xiph.org Foundation nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+* Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+* Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+* Neither the name of ]] .. name .. [[ nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
 
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-DISCLAIMED.  IN NO EVENT SHALL THE FOUNDATION OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
-USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
-ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.]])
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.]])
 end
 
 local function apache(copyright)
@@ -240,26 +239,31 @@ return function(self)
     apache("Copyright 2022 Ronald Ross Miller")
   end)
   library("ogg", "BSD", function()
-    xiphbsd("Copyright (c) 2002, Xiph.org Foundation")
+    bsd3("Copyright (c) 2002, Xiph.org Foundation", "Xiph.org Foundation")
   end)
   library("Opus", "BSD", function()
-    xiphbsd(
-      "Copyright 2001-2011 Xiph.Org, Skype Limited, Octasic, Jean-Marc Valin, Timothy B. Terriberry, CSIRO, Gregory Maxwell, Mark Borgerding, Erik de Castro Lopo")
+    bsd3(
+      "Copyright 2001-2011 Xiph.Org, Skype Limited, Octasic, Jean-Marc Valin, Timothy B. Terriberry, CSIRO, Gregory Maxwell, Mark Borgerding, Erik de Castro Lopo",
+      "Xiph.org Foundation")
   end)
   library("Opusfile", "BSD", function()
-    xiphbsd("Copyright (c) 1994-2013 Xiph.Org Foundation and contributors")
+    bsd3("Copyright (c) 1994-2013 Xiph.Org Foundation and contributors", "Xiph.org Foundation")
   end)
   library("result", "MIT", function()
     mit("Copyright (c) 2017-2021 Matthew Rodusek")
   end)
   library("speexdsp", "BSD", function()
-    xiphbsd(
-      "Copyright 2002-2008 Xiph.org Foundation, Copyright 2002-2008 Jean-Marc Valin, Copyright 2005-2007 Analog Devices Inc., Copyright 2005-2008	Commonwealth Scientific and Industrial Research, Organisation (CSIRO), Copyright 1993, 2002, 2006 David Rowe, Copyright 2003 EpicGames, Copyright 1992-1994	Jutta Degener, Carsten Bormann")
+    bsd3(
+      "Copyright 2002-2008 Xiph.org Foundation, Copyright 2002-2008 Jean-Marc Valin, Copyright 2005-2007 Analog Devices Inc., Copyright 2005-2008	Commonwealth Scientific and Industrial Research, Organisation (CSIRO), Copyright 1993, 2002, 2006 David Rowe, Copyright 2003 EpicGames, Copyright 1992-1994	Jutta Degener, Carsten Bormann",
+      "Xiph.org Foundation")
   end)
   library("tinyfsm", "MIT", function()
     mit("Copyright (c) 2012-2022 Axel Burri")
   end)
   library("tremor", "BSD", function()
-    xiphbsd("Copyright (c) 2002, Xiph.org Foundation")
+    bsd3("Copyright (c) 2002, Xiph.org Foundation", "Xiph.org Foundation")
+  end)
+  library("WavPack", "BSD", function()
+    bsd3("Copyright (c) 1998 - 2006 Conifer Software, All rights reserved.", "Conifer Software")
   end)
 end
