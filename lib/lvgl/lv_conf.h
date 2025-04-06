b077b02a8b94d3012387fe5b77237aa6de104916
Be <be.0@gmx.com>
2025-04-06 02:00:51 -0500

    reduce LVGL allocation from 2 MB to the minimum required.
    This still leaves 171 KB available to LVGL after boot completes.

--- lib/lvgl/lv_conf.h
+++ lib/lvgl/lv_conf.h
@@ -47,7 +47,11 @@
 
 #if LV_USE_STDLIB_MALLOC == LV_STDLIB_BUILTIN
     /*Size of the memory available for `lv_malloc()` in bytes (>= 2kB)*/
-    #define LV_MEM_SIZE (2048U * 1024U)          /*[bytes]*/
+    /*Peak memory usage occurs during font loading on startup when the font files
+     *are copied from flash into buffers in SPIRAM and parsed in parallel.
+     *This size was determined experimentally as the smallest size that does not
+     *crash with the fonts in use.*/
+    #define LV_MEM_SIZE (1390U * 1024U)          /*[bytes]*/
 
     /*Size of the memory expand for `lv_malloc()` in bytes*/
     #define LV_MEM_POOL_EXPAND_SIZE 0
